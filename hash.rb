3c5cb2f29fcfa1e0b7ba94e15a5cf23ee38929fb
doroshenko-e <bekinassoff@gmail.com>
2016-05-16 13:35:28 +0300

    Hash works, but not in all conditions. Text added

--- hash.rb
+++ hash.rb
@@ -6,4 +6,4 @@
 default={'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', 
 	:yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
 
-puts "#{default.keys.grep(/yes/).length}"
+puts "number of keys with 'yes': #{default.keys.grep(/yes/).length}"

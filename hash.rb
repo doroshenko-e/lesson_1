e271dd278f69dbe3b75218b72a2141a73cbb7b2c
doroshenko-e <bekinassoff@gmail.com>
2016-05-16 13:32:15 +0300

    Hash works, but not in all conditions

--- hash.rb
+++ hash.rb
@@ -2,3 +2,8 @@
 #{'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
 #
 #Найти количество ключей, значение которых начинается на "yes"
+
+default={'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', 
+	:yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
+
+puts "#{default.keys.grep(/yes/).length}"

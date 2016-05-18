26c2977353bcbd2a1896d27452e7f62642328d6b
doroshenko-e <bekinassoff@gmail.com>
2016-05-18 07:48:11 +0300

    Array with initialize

--- array.rb
+++ array.rb
@@ -1,27 +1,20 @@
 class Matr
 	#creates matrix
-	def mt
-		martix_numbers=Array.new(4).map{|col| col=Array.new(4).map{|num| num=rand(21)-10}}
-		#matrix_numbers = []
-		#	(0...4).each do |l|
-		#		a[l] = []
-		 # 		(0...4).each do |c|
-		 #		a[l] << rand(0..20)-10
-		 #	end
-		#end
+	def initialize (w, h)
+		@martix_numbers=Array.new(w).map{|col| col=Array.new(h).map{|num| num=rand(21)-10}}
 	end
 	#shows array
 	def show_array
-		our_array=Matr.new.mt	
-		puts "so, array is #{our_array.inspect}"
-		return (our_array)
+		return (@martix_numbers)
 	end
-
 	#shows negative numbers and their sum	
 	def negative
-		negative_numbers=show_array.flatten.reject{|num| num>=0}	
+		negative_numbers=@martix_numbers.flatten.reject{|num| num>=0}	
+		puts "Negative numbers and sum\n"
 		puts "negative numbers #{negative_numbers}"
 		puts "negative elements sum #{negative_numbers.inject(0){|sum, elem| sum+=elem}}"
 	end
-	matrix=Matr.new.negative #entrance point
+	matrix=Matr.new(4, 4) #entrance point
+	puts "Matrix is =>\n#{matrix.show_array}"
+	puts "#{matrix.negative}"
 end

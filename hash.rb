94dadef99cd026b63d9d7665168758abde7951e7
doroshenko-e <bekinassoff@gmail.com>
2016-05-18 10:19:08 +0300

    The truth is out there. Commented code

--- hash.rb
+++ hash.rb
@@ -16,10 +16,16 @@ class Yes
 
 	number = Yes.new
 	puts "Number of 'yes' on the begining of keys is => #{number.num_yes}"
-	#def  num_keys(recur)
-		
-	#	recur < 5 ? num_keys(recur + 1) : (puts "#{recur}")
+	
 
+	# Жутко не уверен в этом коде. Не нравится он мне, хотm убей
+	# На выходе вечная белеберда
+
+	#def  num_keys(recur)
+	#	recur >0 ? num_keys(recur - 1) : (puts "#{recur}")
 	#end
+
+	#num_keys = @default.keys.size
+	#str_arr = Array.new(num_keys){number.num_keys(num).to_s}
 
 end

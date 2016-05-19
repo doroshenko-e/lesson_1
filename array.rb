52dffd5df2579e6e2adfa1aa5842f1906ff96cd6
doroshenko-e <bekinassoff@gmail.com>
2016-05-19 17:44:20 +0300

    Output at the end. No output in methods

--- array.rb
+++ array.rb
@@ -1,20 +1,26 @@
 class Matr
+
 	#creates matrix
 	def initialize (w, h)
 		@martix_numbers=Array.new(w).map{|col| col=Array.new(h).map{|num| num=rand(21)-10}}
 	end
+
 	#shows array
 	def show_array
 		return (@martix_numbers)
 	end
+
 	#shows negative numbers and their sum	
 	def negative
-		negative_numbers=@martix_numbers.flatten.reject{|num| num>=0}	
-		puts "Negative numbers and sum\n"
-		puts "negative numbers #{negative_numbers}"
-		puts "negative elements sum #{negative_numbers.inject(0){|sum, elem| sum+=elem}}"
+		negative_numbers=@martix_numbers.flatten.reject{|num| num>=0}.inject(0){|sum, elem| sum += elem}
 	end
+
 	matrix=Matr.new(4, 4) #entrance point
-	puts "Matrix is =>\n#{matrix.show_array}"
-	puts "#{matrix.negative}"
+
+	# Shows results
+	puts "Matrix is =>\n#{matrix.show_array}\n"
+	puts "Negative numbers and sum\n"
+	puts "Number of negative elements => #{matrix.negative.size}"
+	puts "Negative elements sum => #{matrix.negative}"
+
 end
